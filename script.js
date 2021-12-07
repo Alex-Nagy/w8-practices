4614299e62181aa35e22da0af29c3e79bc3aaa9f
Alex-Nagy <nagyal3x@gmail.com>
2021-12-07 11:00:23 +0100

    filter

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function loadEvent() {
-    console.log('the page has loaded');
-    
+//filter
+
+const ages = [32, 33, 16, 40];
+function testFor() {
+    const resultArr = [];
+    for (const age of ages) {
+        if (age >= 18) {
+            resultArr.push(age);
+        }
+    }
+    return resultArr;
 }
 
-window.addEventListener("load", loadEvent);
+console.log(testFor(),"for ciklus")
+
+const result = ages.filter(checkAdult);
+
+function checkAdult(age) {
+    return age >= 18
+}
+
+console.log(result, "filter")
